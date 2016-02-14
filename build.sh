841fe60d0a50c3c8265428f072112315ffc99d99
Dmitry Shpika <scrptn@gmail.com>
2016-02-14 15:03:06 +0300

    Adjust resulting archives structure

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ build=build
 echo "Processing a full EN version"
 zorba -i -e doc=JMdict_e.xml $src/convert-dictionary.xq > $build/jmdict_eng.json
 echo "Preparing archives"
-tar czf $build/jmdict_eng.json.tgz $build/jmdict_eng.json
-zip $build/jmdict_eng.json.zip $build/jmdict_eng.json
+tar --directory $build -czf $build/jmdict_eng.json.tgz jmdict_eng.json
+zip --junk-paths $build/jmdict_eng.json.zip $build/jmdict_eng.json
 
 echo "Processing EN version with common words only"
 zorba -i -e doc=JMdict_e.xml $src/convert-dictionary-common.xq > $build/jmdict_eng_common.json
 echo "Preparing archives"
-tar czf $build/jmdict_eng_common.json.tgz $build/jmdict_eng_common.json
-zip $build/jmdict_eng_common.json.zip $build/jmdict_eng_common.json
+tar --directory $build -czf $build/jmdict_eng_common.json.tgz jmdict_eng_common.json
+zip --junk-paths $build/jmdict_eng_common.json.zip $build/jmdict_eng_common.json
 
 echo "Done"
