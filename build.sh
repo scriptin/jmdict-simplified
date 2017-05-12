113677278e83c84895c743a444ef6337b493f9b1
Dmitry Shpika <scrptn@gmail.com>
2017-05-12 21:13:19 +0300

    Fail subsequent build steps on errors

--- build.sh
+++ build.sh
@@ -5,22 +5,22 @@ version=$1
 src=src
 build=build
 
-echo "Processing a full EN version"
-zorba -i \
+echo "Processing a full EN version" \
+&& zorba -i \
   -e doc=$doc \
   -e version:=$version \
-  $src/convert-dictionary.xq > $build/jmdict_eng.json
-echo "Preparing archives"
-tar --directory $build -czf $build/jmdict_eng.json.tgz jmdict_eng.json
-zip --junk-paths $build/jmdict_eng.json.zip $build/jmdict_eng.json
+  $src/convert-dictionary.xq > $build/jmdict_eng.json \
+&& echo "Preparing archives" \
+&& tar --directory $build -czf $build/jmdict_eng.json.tgz jmdict_eng.json \
+&& zip --junk-paths $build/jmdict_eng.json.zip $build/jmdict_eng.json
 
-echo "Processing EN version with common words only"
-zorba -i \
+echo "Processing EN version with common words only" \
+&& zorba -i \
   -e doc=$doc \
   -e version:=$version \
-  $src/convert-dictionary-common.xq > $build/jmdict_eng_common.json
-echo "Preparing archives"
-tar --directory $build -czf $build/jmdict_eng_common.json.tgz jmdict_eng_common.json
-zip --junk-paths $build/jmdict_eng_common.json.zip $build/jmdict_eng_common.json
+  $src/convert-dictionary-common.xq > $build/jmdict_eng_common.json \
+&& echo "Preparing archives" \
+&& tar --directory $build -czf $build/jmdict_eng_common.json.tgz jmdict_eng_common.json \
+&& zip --junk-paths $build/jmdict_eng_common.json.zip $build/jmdict_eng_common.json
 
 echo "Done"
