206d91766cf990f87a0ef99907fbaed963222c48
Dmitry Shpika <scrptn@gmail.com>
2023-01-05 17:08:36 +0400

    Fix CommonJS vs ESM issue

--- node/packages/jmdict-simplified-types/copy-types.js
+++ node/packages/jmdict-simplified-types/copy-types.js
@@ -2,21 +2,27 @@
  * @description Copy types from project README into `index.ts`
  */
 
-import { readFileSync, writeFileSync } from 'fs';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+const { readFileSync, writeFileSync } = require('fs');
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const defaultFileOpts = {
+  encoding: 'utf-8',
+};
 
-const readme = readFileSync(`${__dirname}/../../../README.md`, { encoding: 'utf-8' });
-const lines = readme.split("\n");
+const readme = readFileSync(`${__dirname}/../../../README.md`, defaultFileOpts);
+const lines = readme.split('\n');
 
-const typesStart = lines.findIndex(line => /^```(ts|typescript)\s*$/.test(line));
-const typesEnd = lines.findIndex((line, index) => /^```\s*$/.test(line) && index > typesStart);
+const typesStart = lines.findIndex((line) =>
+  /^```(ts|typescript)\s*$/.test(line),
+);
+const typesEnd = lines.findIndex(
+  (line, index) => /^```\s*$/.test(line) && index > typesStart,
+);
 
-const packageJson = readFileSync(`${__dirname}/package.json`, { encoding: 'utf-8' });
+const packageJson = readFileSync(`${__dirname}/package.json`, defaultFileOpts);
 const packageObject = JSON.parse(packageJson);
 
-const typesLines = [`declare module "${packageObject.name}";`].concat(lines.slice(typesStart + 1, typesEnd));
+const typesLines = [`declare module "${packageObject.name}";`].concat(
+  lines.slice(typesStart + 1, typesEnd),
+);
 
-writeFileSync(`${__dirname}/index.ts`, typesLines.join("\n"), { encoding: "utf-8" });
+writeFileSync(`${__dirname}/index.ts`, typesLines.join('\n'), defaultFileOpts);
