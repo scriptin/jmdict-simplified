f54a4486b9675700d0aee062d5529ca0d50322e8
Dmitry Shpika <scrptn@gmail.com>
2023-01-05 17:33:21 +0400

    Use path.join()

--- node/packages/jmdict-simplified-types/copy-types.js
+++ node/packages/jmdict-simplified-types/copy-types.js
@@ -3,12 +3,16 @@
  */
 
 const { readFileSync, writeFileSync } = require('fs');
+const { join } = require('path');
 
 const defaultFileOpts = {
   encoding: 'utf-8',
 };
 
-const readme = readFileSync(`${__dirname}/../../../README.md`, defaultFileOpts);
+const readme = readFileSync(
+  join(__dirname, '..', '..', '..', 'README.md'),
+  defaultFileOpts,
+);
 const lines = readme.split('\n');
 
 const typesStart = lines.findIndex((line) =>
@@ -18,7 +22,10 @@ const typesEnd = lines.findIndex(
   (line, index) => /^```\s*$/.test(line) && index > typesStart,
 );
 
-const packageJson = readFileSync(`${__dirname}/package.json`, defaultFileOpts);
+const packageJson = readFileSync(
+  join(__dirname, 'package.json'),
+  defaultFileOpts,
+);
 const packageObject = JSON.parse(packageJson);
 
 const typesLines = [`declare module "${packageObject.name}";`].concat(
