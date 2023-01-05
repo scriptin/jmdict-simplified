a6582bd3354953af1d5c56123597b050237be3a2
Dmitry Shpika <scrptn@gmail.com>
2023-01-05 23:30:34 +0400

    Remove unused imports

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,6 @@ import de.undercouch.gradle.tasks.download.Download
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.time.ZoneOffset
-import java.util.Locale
 
 group = "org.edrdg.jmdict.simplified"
 version = "3.2.1"
